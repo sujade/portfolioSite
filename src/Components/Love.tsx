ac5faff953024b85cda3e577d035477a0eb012a6
sujade <dnmzsu@gmail.com>
2024-03-26 09:16:43 +0100

    add source code link

--- src/Components/Love.tsx
+++ src/Components/Love.tsx
@@ -3,8 +3,9 @@ import {Container, Row} from "react-bootstrap";
 
 const Love = () => {
     return (
-        <Container className="text-center mt-5">
+        <Container className="text-center mt-5 mb-3">
            <Row><p>Made with lots of <FaHeart/> by Su Jade.</p></Row>
+            <Row><a href="https://github.com/sujade/portfolioSite">Source Code</a></Row>
         </Container>
     );
 };
