104038d082b94e7d67a9dc162be6b2faeae431c0
sujade <dnmzsu@gmail.com>
2024-04-09 14:01:14 +0200

    add new project

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -3,38 +3,55 @@ import CustomCard from "./CustomCard.tsx";
 import GrowthNotes from "../images/pic01.png";
 import thinkBig from "../images/pic02.png";
 import Driftify from "../images/pic03.png";
+import Codedex from "../images/pic04.png";
 
 const Projects = () => {
     return (
-        <Container id="projects" className="py-5 py-md-0 text-center mt-5 shadow-sm rounded-pill border border-black border-5">
+        <Container id="projects"
+                   className="py-5 py-md-0 text-center mt-5 shadow-sm rounded-pill border border-black border-5">
             <Row><h2 className="mt-3">My Projects:</h2>
-                <p>Here, you can see some projects that <br className="d-inline d-md-none"/> I've made or took part in.</p></Row>
+                <p>Here, you can see some projects that <br className="d-inline d-md-none"/> I've made or took part in.
+                </p></Row>
             <Row className="d-flex justify-content-center">
-                <Row className="d-flex justify-content-center gap-5"><CustomCard
-                    title="GrowthNotes"
-                    text="Therapy-specific note-taking application that encourages to user for personal growth and personal care."
-                    image={GrowthNotes}
-                    demoUrl="https://growth-notes.netlify.app/"
-                    repoUrl="https://github.com/sujade/growthnotes"
-                />
+                <Row className="d-flex justify-content-center gap-5">
+                    <Row className="d-flex justify-content-center gap-5">
+                        <CustomCard
+                            title="GrowthNotes"
+                            text="Therapy-specific note-taking application that encourages to user for personal growth and personal care."
+                            image={GrowthNotes}
+                            demoUrl="https://growth-notes.netlify.app/"
+                            repoUrl="https://github.com/sujade/growthnotes"
+                        />
 
-                <CustomCard
-                    title="thinkBig"
-                    text="thinkBig is a project that collaborates with a learning platform to enable learners to interact better."
-                    image={thinkBig}
-                    demoUrl="https://thinkbigproject.netlify.app/"
-                    repoUrl="https://github.com/thinkbig-project/thinkbig"
-                />
+                        <CustomCard
+                            title="thinkBig"
+                            text="thinkBig is a project that collaborates with a learning platform to enable learners to interact better."
+                            image={thinkBig}
+                            demoUrl="https://thinkbigproject.netlify.app/"
+                            repoUrl="https://github.com/thinkbig-project/thinkbig"
+                        />
+                    </Row>
+                    <Row className="d-flex justify-content-center gap-5">
+                        <CustomCard
+                            title="Driftify.se"
+                            text="A website revamp project for the IT Company Driftfiy. The revamp was completely front-end based and design-heavy."
+                            image={Driftify}
+                            demoUrl="https://driftify.se"
+                        />
 
-                <CustomCard
-                    title="Driftify.se"
-                    text="A website revamp project for the IT Company Driftfiy. The revamp was completely front-end based and design-heavy."
-                    image={Driftify}
-                    demoUrl="https://driftify.se"
-                /></Row>
+                        <CustomCard
+                            title="Codédex Personality Test"
+                            text="A personality test that boasts a calming design and quality documentation along with fun value."
+                            image={Codedex}
+                            demoUrl="https://codedex-quiz.vercel.app/"
+                            repoUrl={"https://github.com/sujade/codedex-quiz"}
+                        />
+                    </Row>
+                </Row>
             </Row>
 
-            <Row className="text-center mt-3 pb-3 fs-5"><a href="https://github.com/sujade">Click to see more!</a> </Row>
+            <Row className="text-center mt-3 pb-3 fs-5"><a href="https://github.com/sujade">Click to see more!</a>
+            </Row>
         </Container>
     );
 };
