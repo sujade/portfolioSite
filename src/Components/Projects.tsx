4f5d0085a708c3baf91051da146e88e3d3c4c4a4
sujade <dnmzsu@gmail.com>
2024-04-09 20:33:48 +0200

    change projects pattern and order

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -8,7 +8,7 @@ import Codedex from "../images/pic04.png";
 const Projects = () => {
     return (
         <Container id="projects"
-                   className="py-5 py-md-0 text-center mt-5 shadow-sm border border-black border-5 rounded-5">
+                   className="py-5 py-md-0 text-center mt-5 shadow-sm border border-black border-5 rounded-pill rounded-top">
             <Row><h2 className="mt-3">My Projects:</h2>
                 <p>Here, you can see some projects that <br className="d-inline d-md-none"/> I've made or took part in.
                 </p></Row>
@@ -16,13 +16,20 @@ const Projects = () => {
                 <Row className="d-flex justify-content-center gap-5">
                     <Row className="d-flex justify-content-center gap-5">
                         <CustomCard
+                            title="Codédex Personality Test"
+                            text="A personality test that boasts a calming design and quality documentation along with fun value."
+                            image={Codedex}
+                            demoUrl="https://codedex-quiz.vercel.app/"
+                            repoUrl={"https://github.com/sujade/codedex-quiz"}
+                        />
+
+                        <CustomCard
                             title="GrowthNotes"
                             text="Therapy-specific note-taking application that encourages to user for personal growth and personal care."
                             image={GrowthNotes}
                             demoUrl="https://growth-notes.netlify.app/"
                             repoUrl="https://github.com/sujade/growthnotes"
                         />
-
                         <CustomCard
                             title="thinkBig"
                             text="thinkBig is a project that collaborates with a learning platform to enable learners to interact better."
@@ -30,23 +37,13 @@ const Projects = () => {
                             demoUrl="https://thinkbigproject.netlify.app/"
                             repoUrl="https://github.com/thinkbig-project/thinkbig"
                         />
-
+                    </Row>
+                    <Row className="d-flex justify-content-center gap-5">
                         <CustomCard
                             title="Driftify.se"
                             text="A website revamp project for the IT Company Driftfiy. The revamp was completely front-end based and design-heavy."
                             image={Driftify}
                             demoUrl="https://driftify.se"
-                        />
-                    </Row>
-                    <Row className="d-flex justify-content-center gap-5">
-
-
-                        <CustomCard
-                            title="Codédex Personality Test"
-                            text="A personality test that boasts a calming design and quality documentation along with fun value."
-                            image={Codedex}
-                            demoUrl="https://codedex-quiz.vercel.app/"
-                            repoUrl={"https://github.com/sujade/codedex-quiz"}
                         />
                     </Row>
                 </Row>
